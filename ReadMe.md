c7a3194d1db36428c9968315c33d784461464a03
SerebrovskiyV <vasiliy.serebrovskiy@gmail.com>
2026-04-06 19:26:52 +0200

    initial commit

---
+++ ReadMe.md
@@ -0,0 +1,12 @@
+ServiceDesk-back
+================
+
+Backend part of project Service Desk.
+
+Stack:
+- Spring Boot 3.5.13
+- Java 17 (Temurin)
+- PostgreSQL (Aiven)
+- Spring Security (JWT + OAuth2)
+- Lombok, Liquibase, Spring Data JPA
+- Swagger / OpenAPI
---
+++ src/main/java/com/sitool/servicedesk/ServiceDeskApplication.java
@@ -0,0 +1,13 @@
+package com.sitool.servicedesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceDeskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceDeskApplication.class, args);
+    }
+
+}
